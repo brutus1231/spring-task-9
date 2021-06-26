03766acb2651118c789cc1a0dea1600cd31b1200
Zdzislaw Trzaska <ztrzaska2@gmail.com>
2021-06-26 10:44:38 +0200

    Spring context

--- src/main/java/pl/sda/AuthorBo.java
+++ src/main/java/pl/sda/AuthorBo.java
@@ -3,6 +3,8 @@ package pl.sda;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+
 @Component
 public class AuthorBo {
 
@@ -11,5 +13,10 @@ public class AuthorBo {
     @Autowired
     public AuthorBo(BookBo bookBo) {
         this.bookBo = bookBo;
+    }
+
+    BigDecimal calculateAveragePrice(String pesel) {
+        BigDecimal result = null;
+        return result;
     }
 }
---
+++ src/main/java/pl/sda/Book.java
@@ -0,0 +1,12 @@
+package pl.sda;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class Book {
+
+    private String title;
+    private BigDecimal value;
+}
