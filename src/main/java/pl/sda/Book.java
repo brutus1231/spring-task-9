c0765829e0a402e52d305f1859e2aeb42a30131e
Zdzislaw Trzaska <ztrzaska2@gmail.com>
2021-06-26 10:47:41 +0200

    Spring context

--- src/main/java/pl/sda/Book.java
+++ src/main/java/pl/sda/Book.java
@@ -9,4 +9,9 @@ public class Book {
 
     private String title;
     private BigDecimal value;
+
+    public Book(String title, BigDecimal value) {
+        this.title = title;
+        this.value = value;
+    }
 }
